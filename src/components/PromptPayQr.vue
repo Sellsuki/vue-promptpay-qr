f5cb6b0674dc02b85ce3ea3079602434c65c0c3f
aunn <aunn_it08@hotmail.com>
2017-09-01 02:18:07 +0700

    add qrcode.vue

---
+++ src/components/PromptPayQr.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <qrcode-vue :value="qrString" :size="250"></qrcode-vue>
+  </div>
+</template>
+
+<script>
+  import { encode } from '../libs/ppqrcode.js'
+  import QrcodeVue from 'qrcode.vue'
+
+  export default {
+    props: {
+      id: { type: Number, required: true },
+      amount: { type: Number, required: true }
+    },
+    data () {
+      return {
+        qrString: ''
+      }
+    },
+    mounted () {
+      let mid = {}
+      mid = { nationalId: this.id + '' }
+
+      this.qrString = encode({
+        pointOfInitiation: '11',
+        merchantAccPromptpay: mid,
+        merchant: { countryCode: 'TH' },
+        transaction: { transactionCurrency: '764', transactionAmount: this.amount + '' },
+        additionalData: {},
+        unreserved: {}
+      })
+    },
+    components: {
+      QrcodeVue
+    }
+  }
+</script>
