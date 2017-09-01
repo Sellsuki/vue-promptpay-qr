29f47ebf2b845881475954deba193d36e7c0802b
aunn <aunn_it08@hotmail.com>
2017-09-01 13:18:18 +0700

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <promptpay-qr :id="1234567890123" :amount="500"></promptpay-qr>
+  </div>
+</template>
+
+<script>
+import PromptpayQr from './components/PromptpayQr.vue'
+export default {
+  name: 'app',
+  components: {
+    PromptpayQr
+  }
+}
+</script>
